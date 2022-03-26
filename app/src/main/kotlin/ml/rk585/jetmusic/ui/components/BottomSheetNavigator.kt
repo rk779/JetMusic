8353cb3c37e2922422e9ddf74e86eae49840ecbf
Rahul Krishna <Rahulkrishna585@gmail.com>
2022-03-27 01:16:20 +0530

    app: Use inbuilt `skipHalfExpanded` api for `BottomsheetNavigator`
    Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

--- app/src/main/kotlin/ml/rk585/jetmusic/ui/components/BottomSheetNavigator.kt
+++ app/src/main/kotlin/ml/rk585/jetmusic/ui/components/BottomSheetNavigator.kt
@@ -6,12 +6,9 @@ import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.SwipeableDefaults
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
 import com.google.accompanist.navigation.material.BottomSheetNavigator
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
-import kotlinx.coroutines.flow.collectLatest
 
 @OptIn(
     ExperimentalMaterialNavigationApi::class,
@@ -24,27 +21,9 @@ fun rememberBottomSheetNavigator(
 ): BottomSheetNavigator {
     val sheetState = rememberModalBottomSheetState(
         initialValue = ModalBottomSheetValue.Hidden,
-        animationSpec = animationSpec
+        animationSpec = animationSpec,
+        skipHalfExpanded = skipHalfExpanded
     )
-
-    if (skipHalfExpanded) {
-        LaunchedEffect(sheetState) {
-            snapshotFlow { sheetState.isAnimationRunning }
-                .collectLatest {
-                    with(sheetState) {
-                        val isOpening = currentValue == ModalBottomSheetValue.Hidden
-                                && targetValue == ModalBottomSheetValue.HalfExpanded
-                        val isClosing = currentValue == ModalBottomSheetValue.Expanded
-                                && targetValue == ModalBottomSheetValue.HalfExpanded
-                        when {
-                            isOpening -> animateTo(ModalBottomSheetValue.Expanded)
-                            isClosing -> animateTo(ModalBottomSheetValue.Hidden)
-                        }
-                    }
-                }
-        }
-    }
-
     return remember(sheetState) {
         BottomSheetNavigator(sheetState)
     }
