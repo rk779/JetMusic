25500bd512fb7d13c5a79ab28ba0a01ebf6c19aa
Rahul Krishna <Rahulkrishna585@gmail.com>
2022-06-11 15:57:16 +0530

    ui:artist: Improve `ArtistTopAppBar`
    * Import `ParallaxTopAppBar` from https://android-review.googlesource.com/c/platform/frameworks/support/+/2121933
    
    Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

--- ui/artist/src/main/kotlin/ml/rk585/jetmusic/ui/artist/Artist.kt
+++ ui/artist/src/main/kotlin/ml/rk585/jetmusic/ui/artist/Artist.kt
@@ -1,26 +1,18 @@
 package ml.rk585.jetmusic.ui.artist
 
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.rememberSplineBasedDecay
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -31,50 +23,31 @@ import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarScrollState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.ramcosta.composedestinations.annotation.Destination
 import ml.rk585.jetmusic.core.base.extensions.stripArtistSuffix
 import ml.rk585.jetmusic.ui.common.R
-import ml.rk585.jetmusic.ui.common.components.Loading
 import ml.rk585.jetmusic.ui.common.components.IconButton
-import ml.rk585.jetmusic.ui.common.components.JetMusicTopAppBar
+import ml.rk585.jetmusic.ui.common.components.Loading
 import ml.rk585.jetmusic.ui.common.components.MediaListItem
-import ml.rk585.jetmusic.ui.common.components.Scaffold
+import ml.rk585.jetmusic.ui.common.components.ParallaxTopAppBar
 import ml.rk585.jetmusic.ui.common.components.rememberStateWithLifecycle
-import ml.rk585.jetmusic.ui.common.extensions.copy
-import ml.rk585.jetmusic.ui.common.extensions.drawForegroundGradientScrim
-import ml.rk585.jetmusic.ui.common.extensions.iconButtonBackgroundScrim
 import org.schabi.newpipe.extractor.channel.ChannelInfo
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
 
@@ -116,46 +89,34 @@ internal fun Artist(
     onNavigateUp: () -> Unit
 ) {
     val listState = rememberLazyListState()
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarScrollState())
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
+        decayAnimationSpec = rememberSplineBasedDecay(),
+        state = rememberTopAppBarScrollState()
+    )
 
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
             .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            var appBarHeight by remember { mutableStateOf(0) }
-            val showAppBarBackground by remember {
-                derivedStateOf {
-                    val visibleItemsInfo = listState.layoutInfo.visibleItemsInfo
-                    when {
-                        visibleItemsInfo.isEmpty() -> false
-                        appBarHeight <= 0 -> false
-                        else -> {
-                            val firstVisibleItem = visibleItemsInfo[0]
-                            when {
-                                // If the first visible item is > 0, we want to show the app bar background
-                                firstVisibleItem.index > 0 -> true
-                                // If the first item is visible, only show the app bar background once the only
-                                // remaining part of the item is <= the app bar
-                                else -> firstVisibleItem.size + firstVisibleItem.offset <= appBarHeight
-                            }
-                        }
+            ParallaxTopAppBar(
+                title = { Text(text = channelInfo.name.stripArtistSuffix()) },
+                parallaxImagePainter = rememberAsyncImagePainter(channelInfo.avatarUrl),
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = WindowInsets.statusBars.asPaddingValues(),
+                navigationIcon = {
+                    IconButton(onClick = onNavigateUp) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = stringResource(id = R.string.navigate_up)
+                        )
                     }
-                }
-            }
-
-            ArtistTopAppBar(
-                title = channelInfo.name.stripArtistSuffix(),
-                showAppBarBackground = showAppBarBackground,
-                scrollBehavior = scrollBehavior,
-                onNavigateUp = onNavigateUp,
-                modifier = Modifier
-                    .onSizeChanged { appBarHeight = it.height }
+                },
+                scrollBehavior = scrollBehavior
             )
         }
     ) { contentPadding ->
         ArtistContent(
-            channelInfo = channelInfo,
             channelPagingItems = channelPagingItems,
             listState = listState,
             contentPadding = contentPadding,
@@ -166,7 +127,6 @@ internal fun Artist(
 
 @Composable
 internal fun ArtistContent(
-    channelInfo: ChannelInfo,
     channelPagingItems: LazyPagingItems<StreamInfoItem>,
     listState: LazyListState,
     modifier: Modifier = Modifier,
@@ -175,28 +135,10 @@ internal fun ArtistContent(
     LazyColumn(
         modifier = modifier,
         state = listState,
-        contentPadding = contentPadding.copy(copyTop = false)
+        contentPadding = contentPadding
     ) {
         item {
-            BackdropImage(
-                title = channelInfo.name.stripArtistSuffix(),
-                imageUrl = channelInfo.avatarUrl,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(16f / 10)
-                    .clipToBounds()
-                    .offset {
-                        IntOffset(
-                            x = 0,
-                            y = if (listState.firstVisibleItemIndex == 0) {
-                                listState.firstVisibleItemScrollOffset / 2
-                            } else 0
-                        )
-                    }
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
         }
         item {
             ArtistInfoDetailsHeader(
@@ -211,47 +153,6 @@ internal fun ArtistContent(
                     item = item
                 )
             }
-        }
-    }
-}
-
-@Composable
-internal fun BackdropImage(
-    title: String,
-    imageUrl: String,
-    modifier: Modifier = Modifier
-) {
-    Surface(modifier = modifier) {
-        Box {
-            AsyncImage(
-                model = imageUrl,
-                contentDescription = title,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .drawForegroundGradientScrim(Color.Black.copy(alpha = 0.7f)),
-                contentScale = ContentScale.Crop
-            )
-
-            val textStyle = MaterialTheme.typography.displayMedium
-            val shadowSize = with(LocalDensity.current) {
-                textStyle.fontSize.toPx() / 16
-            }
-
-            Text(
-                text = title,
-                style = textStyle.copy(
-                    color = Color.White,
-                    shadow = Shadow(
-                        color = Color.Black,
-                        offset = Offset(shadowSize, shadowSize),
-                        blurRadius = 0.1f
-                    )
-                ),
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(horizontal = 8.dp)
-            )
         }
     }
 }
@@ -291,50 +192,4 @@ internal fun ArtistInfoDetailsHeader(
             Text(text = stringResource(id = R.string.play))
         }
     }
-}
-
-@Composable
-internal fun ArtistTopAppBar(
-    title: String,
-    showAppBarBackground: Boolean,
-    modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior? = null,
-    onNavigateUp: () -> Unit
-) {
-    val containerColor by animateColorAsState(
-        targetValue = when {
-            showAppBarBackground -> MaterialTheme.colorScheme.surface
-            else -> Color.Transparent
-        },
-        animationSpec = spring(
-            stiffness = Spring.StiffnessLow
-        ),
-    )
-
-    JetMusicTopAppBar(
-        title = {
-            Crossfade(showAppBarBackground) { show ->
-                if (show) Text(text = title)
-            }
-        },
-        modifier = modifier,
-        contentPadding = WindowInsets.systemBars
-            .only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top)
-            .asPaddingValues(),
-        navigationIcon = {
-            IconButton(
-                onClick = onNavigateUp,
-                modifier = Modifier.iconButtonBackgroundScrim(enabled = !showAppBarBackground)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = stringResource(id = R.string.navigate_up)
-                )
-            }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(
-            containerColor = containerColor
-        ),
-        scrollBehavior = scrollBehavior
-    )
 }
---
+++ ui/common/src/main/kotlin/ml/rk585/jetmusic/ui/common/components/ParallaxTopAppBar.kt
@@ -0,0 +1,305 @@
+package ml.rk585.jetmusic.ui.common.components
+
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import ml.rk585.jetmusic.ui.common.extensions.drawForegroundGradientScrim
+import ml.rk585.jetmusic.ui.common.extensions.iconButtonBackgroundScrim
+import kotlin.math.max
+import kotlin.math.roundToInt
+import androidx.compose.ui.text.lerp as textLerp
+
+/**
+ * Top app bar with a background image and a parallax effect.
+ *
+ * @param title the title to be displayed in the top app bar
+ * @param parallaxImagePainter the image painter to be drawn as the top app bar background
+ * @param minHeight the minimum height this top app bar is allowed to collapse to
+ * @param maxHeight the maximum height this top app bar is allowed to expand to
+ * @param modifier the [Modifier] to be applied to this top app bar
+ * @param titleEasing an [Easing] function that defines how the title move on the X and Y axis when
+ * the top app bar is collapsed or expanded
+ * @param navigationIcon the navigation icon displayed at the start of the top app bar. This should
+ * typically be an [IconButton] or [IconToggleButton].
+ * @param actions the actions displayed at the end of the top app bar. This should typically be
+ * [IconButton]s. The default layout here is a [Row], so icons inside will be placed horizontally.
+ * @param colors [TopAppBarColors] that will be used to resolve the colors used for this top app
+ * bar in different states. See [TopAppBarDefaults.smallTopAppBarColors].
+ * @param scrollBehavior a [TopAppBarScrollBehavior] which holds various offset values that will be
+ * applied by this top app bar to set up its height and colors. A scroll behavior is designed to
+ * work in conjunction with a scrolled content to change the top app bar appearance as the content
+ * scrolls. See [TopAppBarScrollBehavior.nestedScrollConnection].
+ */
+@Composable
+fun ParallaxTopAppBar(
+    title: @Composable () -> Unit,
+    parallaxImagePainter: Painter,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(),
+    minHeight: Dp = ParallaxTopAppBarMinHeight,
+    maxHeight: Dp = ParallaxTopAppBarMaxHeight,
+    titleEasing: Easing = LinearEasing,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    colors: TopAppBarColors = TopAppBarDefaults.smallTopAppBarColors(),
+    scrollBehavior: TopAppBarScrollBehavior? = null
+) {
+    val offsetLimit = with(LocalDensity.current) { -maxHeight.toPx() + minHeight.toPx() }
+    SideEffect {
+        if (scrollBehavior?.state?.offsetLimit != offsetLimit) {
+            scrollBehavior?.state?.offsetLimit = offsetLimit
+        }
+    }
+
+    val scrollFraction = scrollBehavior?.scrollFraction ?: 0f
+
+    // We obtain the parallax scroll fraction via the parallaxScrollFraction() extension function.
+    // This fraction computation is always the same, regardless of the behavior that was passed in.
+    // We also coerce the scroll fraction to be slightly larger than zero to prevent any
+    // titleTextStyle sizing issues when expanding the top app bar all the way down due to float
+    // precision.
+    val parallaxScrollFraction =
+        (scrollBehavior?.parallaxScrollFraction()?.coerceIn(0.000001f, 1f) ?: 0f)
+
+    // Obtain the container color from the TopAppBarColors.
+    // This may potentially animate or interpolate a transition between the container-color and the
+    // container's scrolled-color according to the app bar's scroll state.
+    val appBarContainerColor by colors.containerColor(scrollFraction)
+
+    // Wrap the given actions in a Row.
+    val actionsRow = @Composable {
+        Row(
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically,
+            content = actions
+        )
+    }
+
+    // Compose a Surface with a TopAppBarLayout content. The surface's background color will be
+    // animated as specified above, and the height of the app bar will be determined by the current
+    // scroll-state offset.
+    Surface(modifier = modifier, color = appBarContainerColor) {
+        val height = LocalDensity.current.run {
+            maxHeight.toPx() + (scrollBehavior?.state?.offset ?: 0f)
+        }
+        val heightDp = LocalDensity.current.run {
+            height.toDp() + contentPadding.calculateTopPadding()
+        }
+        val minHeightPx = LocalDensity.current.run { minHeight.roundToPx() }
+
+        Box {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(heightDp)
+            ) {
+                Image(
+                    painter = parallaxImagePainter,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .graphicsLayer {
+                            alpha = 1f - parallaxScrollFraction
+                            translationY = -parallaxScrollFraction * 0.05f
+                        }
+                        .drawForegroundGradientScrim(
+                            colors
+                                .containerColor(scrollFraction = scrollFraction)
+                                .value.copy(0.7f)
+                        ),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            ParallaxAppBarLayout(
+                modifier = Modifier.padding(contentPadding),
+                layoutHeight = height.roundToInt(),
+                minHeight = minHeightPx,
+                titleEasingFraction = titleEasing.transform(parallaxScrollFraction),
+                navigationIconContentColor = colors.navigationIconContentColor(scrollFraction).value,
+                titleContentColor = colors.titleContentColor(scrollFraction).value,
+                actionIconContentColor = colors.actionIconContentColor(scrollFraction).value,
+                title = title,
+                titleTextStyle = textLerp(
+                    start = MaterialTheme.typography.headlineMedium,
+                    stop = MaterialTheme.typography.titleLarge,
+                    fraction = parallaxScrollFraction
+                ),
+                navigationIcon = navigationIcon,
+                actions = actionsRow,
+            )
+        }
+    }
+}
+
+/**
+ * Returns a parallax scroll fraction value. The returned value is computed in a similar way for all
+ * any [TopAppBarScrollBehavior].
+ */
+private fun TopAppBarScrollBehavior.parallaxScrollFraction() =
+    if (state.offsetLimit != 0f) state.offset / state.offsetLimit else 0f
+
+@Composable
+private fun ParallaxAppBarLayout(
+    modifier: Modifier,
+    layoutHeight: Int,
+    minHeight: Int,
+    titleEasingFraction: Float,
+    navigationIconContentColor: Color,
+    titleContentColor: Color,
+    actionIconContentColor: Color,
+    title: @Composable () -> Unit,
+    titleTextStyle: TextStyle,
+    navigationIcon: @Composable () -> Unit,
+    actions: @Composable () -> Unit,
+) {
+    val showIconScrim by derivedStateOf { titleEasingFraction != 1f }
+
+    Layout(
+        {
+            Box(
+                Modifier
+                    .layoutId("navigationIcon")
+                    .padding(start = TopAppBarHorizontalPadding)
+                    .iconButtonBackgroundScrim(showIconScrim)
+            ) {
+                CompositionLocalProvider(
+                    LocalContentColor provides navigationIconContentColor,
+                    content = navigationIcon
+                )
+            }
+            Box(
+                Modifier
+                    .layoutId("title")
+                    .padding(horizontal = TopAppBarHorizontalPadding)
+            ) {
+                ProvideTextStyle(value = titleTextStyle) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides titleContentColor,
+                        content = title
+                    )
+                }
+            }
+            Box(
+                Modifier
+                    .layoutId("actionIcons")
+                    .padding(end = TopAppBarHorizontalPadding)
+                    .iconButtonBackgroundScrim(showIconScrim)
+            ) {
+                CompositionLocalProvider(
+                    LocalContentColor provides actionIconContentColor,
+                    content = actions
+                )
+            }
+        },
+        modifier = modifier
+    ) { measurables, constraints ->
+        val navigationIconPlaceable =
+            measurables.first { it.layoutId == "navigationIcon" }
+                .measure(constraints.copy(minWidth = 0))
+        val actionIconsPlaceable =
+            measurables.first { it.layoutId == "actionIcons" }
+                .measure(constraints.copy(minWidth = 0))
+
+        val maxTitleWidth = if (constraints.maxWidth == Constraints.Infinity) {
+            constraints.maxWidth
+        } else {
+            (constraints.maxWidth - navigationIconPlaceable.width - actionIconsPlaceable.width)
+                .coerceAtLeast(0)
+        }
+        val titlePlaceable =
+            measurables.first { it.layoutId == "title" }
+                .measure(constraints.copy(minWidth = 0, maxWidth = maxTitleWidth))
+
+        // Locate the title's baseline.
+        val titleBaseline =
+            if (titlePlaceable[LastBaseline] != AlignmentLine.Unspecified) {
+                titlePlaceable[LastBaseline]
+            } else {
+                0
+            }
+
+        val titleMinX = TopAppBarTitleInset.roundToPx()
+        val titleMaxX = max(TopAppBarTitleInset.roundToPx(), navigationIconPlaceable.width)
+        val titleMinY = (layoutHeight - titlePlaceable.height) / 2
+        val titleMaxY = layoutHeight - titlePlaceable.height - max(
+            TopAppBarTitleBottomPadding.toPx().toInt(),
+            TopAppBarTitleBottomPadding.toPx().toInt() - titlePlaceable.height + titleBaseline
+        )
+
+        layout(constraints.maxWidth, layoutHeight) {
+            // Navigation icon
+            navigationIconPlaceable.placeRelative(
+                x = 0,
+                y = (minHeight - navigationIconPlaceable.height) / 2
+            )
+
+            // Title
+            titlePlaceable.placeRelative(
+                x = lerp(
+                    start = titleMinX,
+                    stop = titleMaxX,
+                    fraction = titleEasingFraction
+                ),
+                y = lerp(
+                    start = titleMaxY,
+                    stop = titleMinY,
+                    fraction = titleEasingFraction
+                )
+            )
+
+            // Action icons
+            actionIconsPlaceable.placeRelative(
+                x = constraints.maxWidth - actionIconsPlaceable.width,
+                y = (minHeight - actionIconsPlaceable.height) / 2
+            )
+        }
+    }
+}
+
+private val ParallaxTopAppBarMinHeight = 64.dp
+private val ParallaxTopAppBarMaxHeight = 180.dp
+private val TopAppBarHorizontalPadding = 4.dp
+private val TopAppBarTitleBottomPadding = 4.dp
+
+// A title inset when the App-Bar is a Medium or Large one. Also used to size a spacer when the
+// navigation icon is missing.
+private val TopAppBarTitleInset = 16.dp - TopAppBarHorizontalPadding
